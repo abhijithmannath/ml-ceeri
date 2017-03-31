c11aaa41c0c7d05b7843c41deca472157838096e
abhijithmannath <abhijithmannath@gmail.com>
2017-03-31 06:07:15 +0530

    search patient

--- session/admin.py
+++ session/admin.py
@@ -3,9 +3,9 @@ from .models import RawSession
 
 # Register your models here.
 class RawSessionAdmin(admin.ModelAdmin):
-	list_display=['patient_id','created']
+	list_display = ['patient_id','session_name','remarks']
 	search_fields=['patient_id']
 
-admin.site.register(RawSession)
+admin.site.register(RawSession, RawSessionAdmin)
 
 # Register your models here.
